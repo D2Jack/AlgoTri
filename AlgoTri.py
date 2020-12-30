afaf1fc411b5290c12744ccd9c578761b632ff0a
D2Jack <73894300+D2Jack@users.noreply.github.com>
2020-12-30 05:36:15 +0100

    Update AlgoTri.py

--- AlgoTri.py
+++ AlgoTri.py
@@ -1,3 +1,5 @@
+##triFusion##
+
 list=[6,4,5,2,1,3,8,9,7]
 def triFusion(list):
   i=len(list)
@@ -22,3 +24,18 @@ def Fusion(list1,list2):
   return Fusion
   
 print(Fusion(list))
+
+##triSelectif##
+
+list=[4,3,8,7,1,2,9,5,6]
+def tri_selection(list):
+    for i in range(len(list)):
+        min = i
+        for j in range(i+1, len(list)):
+            if list[min] > list[j]:
+                min = j
+    tmp = list[i]
+    list[i] = list[min]
+    list[min] = tmp
+    return list
+print (tri_selection(list))
